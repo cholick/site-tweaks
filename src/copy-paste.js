7b8025b9b3426401e840f0afc8d9bba6d758a8ce
Matt Cholick <cholick@gmail.com>
2025-05-02 19:43:58 -0700

    Allow copy and paste
    # Conflicts:
    #	src/manifest.json

---
+++ src/copy-paste.js
@@ -0,0 +1,8 @@
+allowCopyAndPaste = function (e) {
+    e.stopImmediatePropagation();
+    return true;
+};
+
+document.addEventListener("copy", allowCopyAndPaste, true);
+document.addEventListener("paste", allowCopyAndPaste, true);
+document.addEventListener("onpaste", allowCopyAndPaste, true);
